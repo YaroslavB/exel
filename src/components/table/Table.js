da8e3f411fef25cabd8339c43408b8c0007867b9
YaroslavB <yaroslav.bailo@gmail.com>
2023-03-02 18:26:04 +0200

    - code optimization

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
 import {ExelComponent} from '@core/ExelComponent';
 import {createTable} from '@/components/table/table.template';
-import {$} from '@core/Dom';
+import {resizeHandler} from '@/components/table/table.resize';
+import {shouldResize} from '@/components/table/table.functions';
 
 export class Table extends ExelComponent {
   static className ='exel__table'
@@ -16,28 +17,8 @@ export class Table extends ExelComponent {
   }
 
   onMousedown(event) {
-    if (event.target.dataset.resize) {
-      const $resizer = $(event.target)
-      const $parent = $resizer.closest('[data-type="resize"]')
-      const coordinates = $parent.getCords()
-      const type = $resizer.data.resize
-      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
-
-      document.onmousemove = e =>{
-        if (type === 'col') {
-          const delta = e.pageX - coordinates.right
-          const value = coordinates.width + delta
-          $parent.css({width: value + 'px'})
-          cells.forEach(el => el.style.width = value + 'px')
-        } else if (type === 'row') {
-          const delta = e.pageY - coordinates.bottom
-          const value = coordinates.height + delta
-          $parent.css({height: value + 'px'})
-        }
-      }
-      document.onmouseup = () => {
-        document.onmousemove = null
-      }
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event)
     }
   }
 }
---
+++ src/components/table/table.resize.js
@@ -0,0 +1,37 @@
+import {$} from '@core/Dom'
+
+export function resizeHandler($root, event) {
+  const $resizer = $(event.target)
+  const $parent = $resizer.closest('[data-type="resize"]')
+  const coordinates = $parent.getCords()
+  const type = $resizer.data.resize
+  const siteProp = type === 'col' ? 'bottom' : 'right'
+  let value
+
+  $resizer.css({opacity: 1, [siteProp]: '-4000px'})
+  document.onmousemove = e =>{
+    if (type === 'col') {
+      const delta = e.pageX - coordinates.right
+      value = coordinates.width + delta
+      $resizer.css({right: -delta+ 'px'})
+    } else if (type === 'row') {
+      const delta = e.pageY - coordinates.bottom
+      value = coordinates.height + delta
+      $resizer.css({bottom: -delta+ 'px'})
+    }
+  }
+  document.onmouseup = () => {
+    document.onmousemove = null
+    document.onmouseup = null
+
+    if (type === 'col') {
+      $parent.css({width: value + 'px'})
+      $root.findAll(`[data-col="${$parent.data.col}"]`)
+          .forEach(el => el.style.width = value + 'px')
+    } else {
+      $parent.css({height: value + 'px'})
+    }
+
+    $resizer.css({opacity: 0, bottom: 0, right: 0})
+  }
+}
