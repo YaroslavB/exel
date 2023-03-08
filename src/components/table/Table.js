b82ceb6016884298678b0def0dd8607a8f067623
YaroslavB <yaroslav.bailo@gmail.com>
2023-03-08 19:46:10 +0200

    -  element selection on click

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,8 +1,9 @@
 import {ExelComponent} from '@core/ExelComponent';
 import {createTable} from '@/components/table/table.template';
 import {resizeHandler} from '@/components/table/table.resize';
-import {shouldResize} from '@/components/table/table.functions';
+import {isCell, shouldResize} from '@/components/table/table.functions';
 import {TableSelection} from './TableSelection';
+import {$} from '@core/Dom'
 
 export class Table extends ExelComponent {
   static className ='exel__table'
@@ -30,6 +31,9 @@ export class Table extends ExelComponent {
   onMousedown(event) {
     if (shouldResize(event)) {
       resizeHandler(this.$root, event)
+    } else if (isCell(event)) {
+      const $target = $(event.target)
+      this.selection.select($target)
     }
   }
 }
